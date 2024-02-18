4dfe881c36f01488aa586380e05cea20ce0083a8
statcat2017 <b33jay@gmail.com>
2024-02-18 12:56:34 +0000

    Problem 1 solved in C.

---
+++ c/prob-1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main()
+    {
+        int total = 0;
+
+        for (int i = 0; i <1000; i++)
+        {
+            if (i % 3 == 0||i % 5 == 0) {total += i;}
+        }
+
+        printf("Final total: %i\n", total);
+    }
+
+
